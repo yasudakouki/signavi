86ceb73e2dfd78e82e85fcad7f2f4c890d385fd4
yasudakouki <e225703@ie.u-ryukyu.ac.jp>
2024-11-19 15:11:21 +0900

    Initial Commit

---
+++ signavi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  signavi
+//
+//  Created by 安田光究 on 2024/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ signavi/signaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  signaviApp.swift
+//  signavi
+//
+//  Created by 安田光究 on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct signaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
