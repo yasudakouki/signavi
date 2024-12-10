b3220c2bd1836475c59d918a5e2f8b3373a81b08
applerm <e225725@ie.u-ryukyu.ac.jp>
2024-12-10 20:32:21 +0900

    設定の文字変えた

--- signavi/SettingsView.swift
+++ signavi/SettingsView.swift
@@ -1,19 +1,23 @@
 import SwiftUI
 
 struct SettingView: View {
-    @State private var isAlarmOn: Bool = false
+    @State private var draw_rectangle: Bool = false
     @State private var isVibrationOn: Bool = false
     
     var body: some View {
         Form {
-            Section(header: Text("Alarm:")) {
-                Toggle("Alarm Sound", isOn: $isAlarmOn)
-                Toggle("Vibration", isOn: $isVibrationOn)
+            Section(header: Text("View setting")) {
+                Toggle("draw rectangle", isOn: $draw_rectangle)
+                //Toggle("Vibration", isOn: $isVibrationOn)
             }
             Section(header: Text("Save:")) {
                 Button(action: {
+                    /*
                     print("Settings Saved: Alarm - \(isAlarmOn), Vibration - \(isVibrationOn)")
-                }) {
+                     */
+                    print("save was taped")
+                })
+                {
                     HStack {
                         Spacer()
                         Text("Done")
